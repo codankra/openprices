097dfecfcb3377777d88a76c3a41cbae6e0013cb
Daniel Kramer <codankra@gmail.com>
2024-08-27 01:27:20 -0500

    Google and Github OAuth

---
+++ app/auth.server.ts
@@ -0,0 +1,52 @@
+import { createCookieSessionStorage } from "@remix-run/node";
+import { Authenticator, AuthorizationError } from "remix-auth";
+import { GitHubStrategy } from "remix-auth-github";
+import { GoogleStrategy } from "remix-auth-google";
+
+export const sessionStorage = createCookieSessionStorage({
+  cookie: {
+    name: "__session",
+    httpOnly: true,
+    path: "/",
+    sameSite: "lax",
+    secrets: [process.env.AUTH_COOKIE_SECRET!],
+    secure: process.env.NODE_ENV === "production",
+  },
+});
+
+export const auth = new Authenticator<string>(sessionStorage);
+
+auth.use(
+  new GitHubStrategy(
+    {
+      clientId: process.env.AUTH_GITHUB_CLIENT_ID!,
+      clientSecret: process.env.AUTH_GITHUB_CLIENT_SECRET!,
+      redirectURI: "http://localhost:3000/auth/gh/callback",
+    },
+    async ({ profile, tokens, request, context }) => {
+      // Here you should create or find a user in your database
+      return profile.id;
+    }
+  )
+);
+
+auth.use(
+  new GoogleStrategy(
+    {
+      clientID: process.env.AUTH_GOOGLE_CLIENT_ID!,
+      clientSecret: process.env.AUTH_GOOGLE_CLIENT_SECRET!,
+      callbackURL: "http://localhost:3000/auth/g/callback",
+    },
+    async ({
+      profile,
+      request,
+      accessToken,
+      refreshToken,
+      extraParams,
+      context,
+    }) => {
+      // Here you should create or find a user in your database
+      return profile.id;
+    }
+  )
+);
--- app/entry.server.tsx
+++ app/entry.server.tsx
@@ -18,11 +18,7 @@ export default function handleRequest(
   request: Request,
   responseStatusCode: number,
   responseHeaders: Headers,
-  remixContext: EntryContext,
-  // This is ignored so we can keep it in the template for visibility.  Feel
-  // free to delete this parameter in your app if you're not using it!
-  // eslint-disable-next-line @typescript-eslint/no-unused-vars
-  loadContext: AppLoadContext
+  remixContext: EntryContext
 ) {
   return isbot(request.headers.get("user-agent") || "")
     ? handleBotRequest(
