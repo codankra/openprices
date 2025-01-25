7785244aa1a2976dd57310a3d42cff6e05f3a700
Daniel Kramer <codankra@gmail.com>
2025-01-25 01:59:05 -0600

    vite dynamic site name w/ local dev tunnel

--- vite.config.ts
+++ vite.config.ts
@@ -4,6 +4,7 @@ import tsconfigPaths from "vite-tsconfig-paths";
 
 export default defineConfig({
   server: {
+    allowedHosts: [process.env.SITE_NAME!.replace(/^https?:\/\//, "")],
     port: 3000,
   },
   css: {
