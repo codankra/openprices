517ea38bfbb62b48ed736dab66e283c331227015
Daniel Kramer <codankra@gmail.com>
2026-07-15 19:54:42 -0500

    v8 flags

--- react-router.config.ts
+++ react-router.config.ts
@@ -1,4 +1,8 @@
 import type { Config } from "@react-router/dev/config";
 export default {
   prerender: ["/"],
+  future: {
+    v8_splitRouteModules: true,
+    v8_viteEnvironmentApi: true,
+  },
 } satisfies Config;
