a6d9fd119784e1255494956ed07a448387133523
Daniel Kramer <codankra@gmail.com>
2026-09-01 22:55:22 -0500

    Explicitly declare openpricedata.com as primary site name

--- react-router.config.ts
+++ react-router.config.ts
@@ -3,9 +3,13 @@ import type { Config } from "@react-router/dev/config";
 const siteHost = process.env.SITE_NAME
   ? new URL(process.env.SITE_NAME).host
   : undefined;
+const allowedActionOrigins = [
+  "openpricedata.com",
+  ...(siteHost ? [siteHost] : []),
+];
 
 export default {
   prerender: ["/"],
-  allowedActionOrigins: [...(siteHost ? [siteHost] : [])],
+  allowedActionOrigins,
   splitRouteModules: true,
 } satisfies Config;
